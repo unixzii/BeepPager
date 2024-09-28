b96f09309376af4f09cc5467f600a684a37bffb1
Cyandev <unixzii@gmail.com>
2024-09-29 00:35:14 +0800

    Close WebSocket connections gracefully

--- server/src/handler.rs
+++ server/src/handler.rs
@@ -41,56 +41,63 @@ async fn send_ws_message(msg: OutgoingMessage, ws: &mut ws::WebSocket) -> Result
     Ok(())
 }
 
+async fn serve_ws_connection(mut ws: ws::WebSocket, state: Arc<AppState>) -> ws::WebSocket {
+    let state = state;
+    let conn_handle = state.register_connection().await;
+    let conn_id = conn_handle.id();
+
+    let mut event_chan = conn_handle
+        .take_event_channel()
+        .await
+        .expect("should get the channel");
+
+    loop {
+        select! {
+            evt = event_chan.recv() => {
+                let Some(evt) = evt else {
+                    return ws;
+                };
+
+                debug!("[{conn_id}] sending event: {evt:?}");
+                let res = send_ws_message(evt, &mut ws).await;
+                if let Err(err) = res {
+                    error!("[{conn_id}] error occurred while sending event: {err}");
+                    return ws;
+                }
+            },
+            msg = ws.recv() => {
+                let Some(msg_res) = msg else {
+                    return ws;
+                };
+
+                let msg = match msg_res {
+                    Ok(msg) => msg,
+                    Err(err) => {
+                        error!("[{conn_id}] error occurred while receiving msg: {err}");
+                        return ws;
+                    }
+                };
+
+                let res = handle_ws_message(msg, &conn_handle, &state).await;
+                if let Err(err) = res {
+                    error!("[{conn_id}] error occurred while handling msg: {err}");
+                    return ws;
+                }
+            },
+        }
+    }
+}
+
 pub async fn ws_handler(
     ws: WebSocketUpgrade,
     State(state): State<Arc<AppState>>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
 ) -> impl IntoResponse {
     info!("got connection from {addr}");
-    ws.on_upgrade(|mut ws| async move {
-        let state = state;
-        let conn_handle = state.register_connection().await;
-        let conn_id = conn_handle.id();
+    ws.on_upgrade(move |ws| async move {
+        let ws = serve_ws_connection(ws, state).await;
 
-        let mut event_chan = conn_handle
-            .take_event_channel()
-            .await
-            .expect("should get the channel");
-
-        loop {
-            select! {
-                evt = event_chan.recv() => {
-                    let Some(evt) = evt else {
-                        return;
-                    };
-
-                    debug!("[{conn_id}] sending event: {evt:?}");
-                    let res = send_ws_message(evt, &mut ws).await;
-                    if let Err(err) = res {
-                        error!("[{conn_id}] error occurred while sending event: {err}");
-                        return;
-                    }
-                },
-                msg = ws.recv() => {
-                    let Some(msg_res) = msg else {
-                        return;
-                    };
-
-                    let msg = match msg_res {
-                        Ok(msg) => msg,
-                        Err(err) => {
-                            error!("[{conn_id}] error occurred while receiving msg: {err}");
-                            return;
-                        }
-                    };
-
-                    let res = handle_ws_message(msg, &conn_handle, &state).await;
-                    if let Err(err) = res {
-                        error!("[{conn_id}] error occurred while handling msg: {err}");
-                        return;
-                    }
-                },
-            }
-        }
+        info!("closing connection from {addr}");
+        _ = ws.close().await;
     })
 }
